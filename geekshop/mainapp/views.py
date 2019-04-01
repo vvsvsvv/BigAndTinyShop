221b49b5fb94a93cb79564fd1dc85161898e850c
vicsergev <v.s.sergevnin@gmail.com>
2019-04-02 01:29:26 +0300

    - added uploading database to json file for debugging purposes

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 from mainapp.models import ProductCategory, Product
+import json
+
+def upload_db():
+    # function for uploading db data to json
+    categories = ProductCategory.objects.all()
+
+    with open('categories.json', 'w', encoding='utf-8') as categories_json:
+        for category in categories:
+            json.dump(dict(filter(lambda x: not x[0].startswith('_'), category.__dict__.items())), categories_json)
+
+    products = Product.objects.all()
+
+    with open('products.json', 'w', encoding='utf-8') as products_json:
+        for product in products:
+            json.dump(dict(filter(lambda x: not x[0].startswith('_'), product.__dict__.items())), products_json, default=float)
 
 def index(request):
     context={
@@ -12,6 +27,8 @@ def index(request):
 def catalog(request):
 
     categories = []
+
+    # upload_db()
 
     # Подгрузка изображения первого представителя категории в качестве обложки категории
     for category in ProductCategory.objects.all():
