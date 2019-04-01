87f170a6048ca19ba7ea9cceb99caa1194d807ae
vicsergev <v.s.sergevnin@gmail.com>
2019-04-01 23:23:17 +0300

    - modified database schema and site logo

--- geekshop/mainapp/models.py
+++ geekshop/mainapp/models.py
@@ -17,11 +17,17 @@ class Product(models.Model):
         (2, 'женский')
     )
 
+    AGE_CHOICES = (
+        (0, 'взрослый'),
+        (1, 'детский')
+    )
+
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='имя продукта', max_length=128)
     image = models.ImageField(upload_to='products_images', blank=True)
     description = models.TextField(verbose_name='описание продукта', blank=True)
     sex = models.SmallIntegerField(verbose_name='пол', default=0, choices=GENDER_CHOICES)
+    age = models.SmallIntegerField(verbose_name='возраст', default=0, choices=AGE_CHOICES)
     price = models.DecimalField(verbose_name='цена продукта', max_digits=8, decimal_places=2, default=0)
     quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
 
