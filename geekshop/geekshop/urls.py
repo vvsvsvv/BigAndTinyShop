568ce9c2798b120a1b161206a88e46a87e823c57
vicsergev <v.s.sergevnin@gmail.com>
2019-04-04 00:58:09 +0300

    - added 'main' namespace using

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -14,15 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import mainapp.views as mainapp
 
 urlpatterns = [
-    path('', mainapp.index, name='index'),
-
-    path('catalog/', mainapp.catalog, name='catalog'),
-
-    path('contacts/', mainapp.contacts, name='contacts'),
+    path('', include('mainapp.urls', namespace='main')),
 
     path('admin/', admin.site.urls),
 ]
---
+++ geekshop/mainapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', mainapp.index, name='index'),
+
+    path('catalog/', mainapp.catalog, name='catalog'),
+
+    path('contacts/', mainapp.contacts, name='contacts'),
+]
