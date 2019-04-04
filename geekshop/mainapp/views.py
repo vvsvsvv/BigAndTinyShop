4ee0138373f1cde955cc2b3aebf9a3c577d9a883
vicsergev <v.s.sergevnin@gmail.com>
2019-04-05 00:07:10 +0300

    - modified upload_db function. items are uploaded as a list now

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -6,22 +6,30 @@ import json
 def upload_db():
     # function for uploading db data to json
     categories = ProductCategory.objects.all()
+    categories_list = []
 
     with open('categories.json', 'w', encoding='utf-8') as categories_json:
         for category in categories:
-            json.dump(dict(filter(lambda x: not x[0].startswith('_'), category.__dict__.items())), categories_json)
+            categories_list.append(dict(filter(lambda x: not x[0].startswith('_'), category.__dict__.items())))
+        json.dump(categories_list, categories_json)
+
 
     subcategories = ProductSubCategory.objects.all()
+    subcategories_list = []
 
     with open('subcategories.json', 'w', encoding='utf-8') as subcategories_json:
         for subcategory in subcategories:
-            json.dump(dict(filter(lambda x: not x[0].startswith('_'), subcategory.__dict__.items())), subcategories_json)
+            subcategories_list.append(dict(filter(lambda x: not x[0].startswith('_'), subcategory.__dict__.items())))
+        json.dump(subcategories_list, subcategories_json)
 
     products = Product.objects.all()
+    product_list = []
 
     with open('products.json', 'w', encoding='utf-8') as products_json:
         for product in products:
-            json.dump(dict(filter(lambda x: not x[0].startswith('_'), product.__dict__.items())), products_json, default=float)
+            product_list.append(dict(filter(lambda x: not x[0].startswith('_'), product.__dict__.items())))
+        json.dump(product_list, products_json, default=float)
+
 
 def index(request):
     context={
