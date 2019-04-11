59fd4b3545819751d1475e2e3568dd64326781f1
vicsergev <v.s.sergevnin@gmail.com>
2019-04-11 18:22:42 +0300

    - adminapp index page

--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from authapp.models import ShopUser
+
 
 def index(request):
-    pass
+    title = 'админка/пользователи'
+
+    users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser',
+                                                 '-is_staff', 'username')
+
+    context = {
+        'title': title,
+        'objects': users_list
+    }
+
+    return render(request, 'adminapp/index.html', context)
