2a7a329eb392d3ea81f66eb2c7213a3b48b5f7c7
vicsergev <v.s.sergevnin@gmail.com>
2019-04-14 20:23:04 +0300

    - adminapp products page

--- geekshop/adminapp/urls.py
+++ geekshop/adminapp/urls.py
@@ -6,5 +6,6 @@ app_name = 'adminapp'
 urlpatterns = [
     path('', adminapp.index, name='index'),
     path('categories/', adminapp.categories, name='categories'),
-    path('subcategories/<int:pk>', adminapp.subcategories, name='subcategories'),
+    path('subcategories/<int:pk>/', adminapp.subcategories, name='subcategories'),
+    path('products/<int:category_pk>/<int:subcategory_pk>/', adminapp.products, name='products'),
 ]
--- geekshop/adminapp/views.py
+++ geekshop/adminapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from authapp.models import ShopUser
-from mainapp.models import ProductCategory, ProductSubCategory
+from mainapp.models import ProductCategory, ProductSubCategory, Product
 
 
 def index(request):
@@ -34,7 +34,7 @@ def subcategories(request, pk):
 
     title = f'админка/подкатегории ({category.name})'
 
-    object_list = ProductSubCategory.objects.filter(category__pk=pk).order_by('name')
+    object_list = category.productsubcategory_set.all().order_by('name')
 
     context = {
         'title': title,
@@ -42,3 +42,21 @@ def subcategories(request, pk):
         'object_list': object_list
     }
     return render(request, 'adminapp/productsubcategory_list.html', context)
+
+
+def products(request, category_pk, subcategory_pk):
+
+    category = get_object_or_404(ProductCategory, pk=category_pk)
+    subcategory = get_object_or_404(ProductSubCategory, pk=subcategory_pk)
+
+    title = f'админка/товары ({subcategory.name} - {category.name})'
+
+    object_list = subcategory.product_set.all().order_by('name')
+
+    context = {
+        'title': title,
+        'category': category,
+        'subcategory': subcategory,
+        'object_list': object_list
+    }
+    return render(request, 'adminapp/product_list.html', context)
