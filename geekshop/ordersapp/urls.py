48b66dc019e657ea97733bf64c8a8705231f2e31
vicsergev <v.s.sergevnin@gmail.com>
2020-04-11 20:01:22 +0300

    order table template, model and form files for order

--- geekshop/ordersapp/urls.py
+++ geekshop/ordersapp/urls.py
@@ -4,7 +4,7 @@ import ordersapp.views as ordersapp
 app_name = 'ordersapp'
 
 urlpatterns = [
-    path('', ordersapp.index, name='index'),
+    path('', ordersapp.OrdersList.as_view(), name='orders_list'),
     # path('add/<int:pk>/', ordersapp.add, name='add'),
     # path('remove/<int:pk>/', ordersapp.remove, name='remove'),
     # path('update/<int:pk>/<int:quantity>/', ordersapp.update),
--- geekshop/ordersapp/views.py
+++ geekshop/ordersapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from ordersapp.models import Order
 
-# Create your views here.
 
-def index(request):
-    pass
+class OrdersList(ListView):
+    model = Order
+    
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
